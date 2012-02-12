5d288ff62bfc947ef4f4e971cb92b10b59f71886
Dedi Purwanto <kecebongsoft@xxxxx.(none)>
2012-02-12 12:54:24 +0800

    Basic main mockup

--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.contrib import admin
 admin.autodiscover()
@@ -11,11 +12,4 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
 if settings.DEBUG:
-    urlpatterns += patterns('',
-        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
-            'document_root': settings.MEDIA_ROOT,
-        }),
-        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
-            'document_root': settings.STATIC_ROOT,
-        }),
-    )
+    urlpatterns += staticfiles_urlpatterns()
